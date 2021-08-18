a6ee7923ca2f1adb46607545c1e7a08fdcf4829f
Ravi-FEHCJ-Developer <ravi96it@gmail.com>
2021-08-18 03:25:50 -0700

    readme

---
+++ BloodBank/readme/readme.txt
@@ -0,0 +1,4 @@
+I used multiple libraries in my project.
+1) jQuery ---> help to do some tasks more easily and effeciently.
+2) Angular Material ---> It have some package which are good for drag and drop task to perform. Example CdkDragStart, CdkDragMove, CdkDragDrop, moveItemInArray, copyArrayItem, transferArrayItem
+3) bootstrap and CSS ---> To make page more responsive.
--- BloodBank/src/app/app.component.ts
+++ BloodBank/src/app/app.component.ts
@@ -1,13 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatList } from '@angular/material/list';
-
-import {
-  CdkDragStart,
-  CdkDragMove,
-  CdkDragDrop,
-  moveItemInArray,
-  copyArrayItem
-} from '@angular/cdk/drag-drop';
+import {  CdkDragStart, CdkDragMove, CdkDragDrop, moveItemInArray, copyArrayItem, transferArrayItem } from '@angular/cdk/drag-drop';
 
 declare var $: any;
 
@@ -18,83 +11,89 @@ declare var $: any;
 })
 export class AppComponent 
 {
-  // @ViewChild(MatList, { read: ElementRef }) 
-  // child: any = ElementRef;
 
-  // _currentIndex : any;
-  // _currentField : any;
+  @ViewChild(MatList, { read: ElementRef }) child: ElementRef;
 
-  // Blood_bottles = ['boolean', 'string', 'text', 'user'];
+  _currentIndex: any;
+  _currentField: any;
 
-  // fields: string[] = [];
-  // boxStyle : boolean = true;
+  fields: string[] = [];
+  remaining_bottle: any;
 
-  title = 'BloodBank';
-  Blood_bottles = [ 
-    { bg:'A+', count:2 , className:'Aplus'},
-    { bg:'B+', count:5 , className:'Bplus'},
-    { bg:'AB+', count:6 , className:'ABplus'},
-    { bg:'O+', count:7 , className:'Oplus'},
-    { bg:'A-', count:2 , className:'Aminus'}, 
-    { bg:'B-', count:1 , className:'Bminus'},
-    { bg:'AB-', count:2 , className:'ABminus'},
-    { bg:'O-', count:2 , className:'Ominus'},
+  Blood_bottles = [
+    { bg: 'A+', count: 2, className: 'Aplus' },
+    { bg: 'B+', count: 5, className: 'Bplus' },
+    { bg: 'AB+', count: 6, className: 'ABplus' },
+    { bg: 'O+', count: 7, className: 'Oplus' },
+    { bg: 'A-', count: 2, className: 'Aminus' },
+    { bg: 'B-', count: 1, className: 'Bminus' },
+    { bg: 'AB-', count: 2, className: 'ABminus' },
+    { bg: 'O-', count: 2, className: 'Ominus' }
   ];
 
-  // Blood_bottles = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ];
-  pos : number = 0;
-  onChange(event:Event) 
-  {
-    let a = "." + (event.target as HTMLInputElement).value
-    $(a).addClass("highlight");
-    $(a).parent().siblings().children().removeClass("highlight");
-    const found = this.Blood_bottles.some(el => el.className === (event.target as HTMLInputElement).value);
-    console.log(found)
-    this.pos = this.Blood_bottles.map(function (e) {
+  pos: number = 0;
+  onChange(event: Event) {
+    let a = '.' + (event.target as HTMLInputElement).value;
+    $(a).addClass('highlight');
+    $(a)
+      .parent()
+      .siblings()
+      .children()
+      .removeClass('highlight');
+    const found = this.Blood_bottles.some(
+      el => el.className === (event.target as HTMLInputElement).value
+    );
+    console.log(found);
+    this.pos = this.Blood_bottles.map(function(e) {
       return e.className;
     }).indexOf((event.target as HTMLInputElement).value);
   }
-  onChangenumberofbottles(event : Event)
-  {
-    let remaining_bottle =  this.Blood_bottles[this.pos].count - parseInt((event.target as HTMLInputElement).value) ;
-    console.log("remaining_bottle = " + remaining_bottle)
-    this.Blood_bottles[this.pos].count = remaining_bottle;
+  onChangenumberofbottles(event: Event) {
+    this.remaining_bottle =
+      this.Blood_bottles[this.pos].count -
+      parseInt((event.target as HTMLInputElement).value);
+    console.log('remaining_bottle = ' + this.remaining_bottle);
   }
 
+  Store_Remaining_bottle() {
+    this.Blood_bottles[this.pos].count = this.remaining_bottle;
+  }
 
-  // dragStart(event: Event) {
-  //   this._currentIndex = this.Blood_bottles[0].bg.indexOf(
-  //     (event.target as HTMLInputElement).value
-  //   );
-  //   // Get index of dragged type
-  //   this._currentField = this.child.nativeElement.children[this._currentIndex];
-  //   // Store HTML field
-  // }
+  dragStart(event: CdkDragStart) {
+    // console.log(event.source.data)
+    this._currentIndex = this.Blood_bottles.indexOf(event.source.data); // Get index of dragged type
+    // console.log(this._currentIndex)
+    this._currentField = this.child.nativeElement.children[event.source.data]; // Store HTML field
+    console.log(this._currentField);
+  }
 
-  // moved(event: Event) {
-  //   // Check if stored HTML field is as same as current field
-  //   if (
-  //     this.child.nativeElement.children[this._currentIndex] !==
-  //     this._currentField
-  //   ) {
-  //     // Replace current field, basically replaces placeholder with old HTML content
-  //     this.child.nativeElement.replaceChild(
-  //       this._currentField,
-  //       this.child.nativeElement.children[this._currentIndex]
-  //     );
-  //   }
-  // }
+  moved(event: CdkDragMove) {
+    // Check if stored HTML field is as same as current field
+    if (
+      this.child.nativeElement.children[this._currentIndex] !==
+      this._currentField
+    ) {
+      // Replace current field, basically replaces placeholder with old HTML content
+      this.child.nativeElement.replaceChild(
+        this._currentField,
+        this.child.nativeElement.children[this._currentIndex]
+      );
+    }
+  }
 
-  // itemDropped(event: CdkDragDrop<any[]>) {
-  //   this.fields = [];
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(this.fields, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     this.addField(event.item.data, event.currentIndex);
-  //   }
-  // }
+  itemDropped(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.fields, event.previousIndex, event.currentIndex);
+      console.log(event.previousIndex)
+      console.log(event.currentIndex)
+    } else {
+      this.addField(event.item.data, event.currentIndex);
+    }
+    this.Store_Remaining_bottle();
+  }
 
-  // addField(fieldType: string, index: number) {
-  //   this.fields.splice(index, 0, fieldType);
-  // }
+  addField(fieldType: string, index: number) {
+    this.fields.splice(index, 0, fieldType);
+  }
 }
+
